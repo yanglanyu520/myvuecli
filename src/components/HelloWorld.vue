ebe8b0f2b4a8fd5ea05a51877f2d1851f4ea8c62
yanglanyu520 <1536679333@qq.com>
2019-09-12 09:15:38 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+   <router-link to="newcontact"><h1>{{ msg }}</h1></router-link>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'welcome'
+    }
+   },
+}
+</script>
+<style lang="scss" scoped>
+ h1{
+     font-weight: normal;
+     font-size: 18px;
+   }
+</style>
